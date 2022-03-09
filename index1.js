29277e83ad98cbb7fa5b6dd298f6207715103eac
Yuliia Holub <140198j@gmail.com>
2022-03-09 19:32:35 +0200

    regex variation

--- index1.js
+++ index1.js
@@ -8,39 +8,26 @@
 */
 
 const [input1, input2] = document.querySelectorAll('input');
-const symbols =['`','!','@','#', '$', '%','^', '&', '*', '(', ')',';']
+// const symbols =['`','!','@','#', '$', '%','^', '&', '*', '(', ')',';']
+const regexPassword = new RegExp(/\b\w{5,}[!-\/,:-?,\[-`]{1,}\w{1,}\b/)
 
 input1.addEventListener('blur', validateEmail);
 input2.addEventListener('blur', validatePassword);
 
 
-function validatePassword(event){
-    
-    const inputValue = event.target.value.split('');
-    // console.log(inputValue)
-    if(inputValue.length >= 5){
-        inputValue.some(
-            (element) => {
-                for(const item of symbols){
-                    if(element === item){
-                       return event.target.classList.add('valid');
-                    }
-                }
-                return event.target.classList.add('invalid')
-            }
-        )
+function validatePassword({ target, target: { value: password } }) {
+    if (password.search(/\b\w{5,}[!-\/,:-?,\[-`]{1,}\w{1,}\b/))  {
+      target.classList.add('valid')
     } else{
-        event.target.classList.add('invalid');
+      target.classList.add('invalid');
     }
-    
-};
+}
 
-function validateEmail(event){
-    const emailArray = event.target.value.split('');
-    if(emailArray.includes('@' && '.')){
-        event.target.classList.add('valid');
+
+function validateEmail({target, target: {value: email}}) {
+    if (email.search(/\w{4-14}\b@[a-z]{2,7}\.[a-z]{1,3}/)) {
+        target.classList.add('valid')
     } else{
-        event.target.classList.add('invalid');
+         target.classList.add('invalid');
     }
-    
 }
